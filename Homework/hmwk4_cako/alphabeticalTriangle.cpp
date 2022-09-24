196af4c639a85a4b517e0f820af71b6a16cb7eb2
Tyler Cako <tylercako@resgw2-192-222-dhcp.int.colorado.edu>
2022-09-23 22:55:22 -0600

    initial commit

---
+++ Homework/hmwk4_cako/alphabeticalTriangle.cpp
@@ -0,0 +1,42 @@
+// CSCI 1300 Fall 2022
+// Author: Tyler Cako
+// Recitation: 308 – Baljot Kaur
+// Homework 4 - Problem #5
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // INput validation
+    int height = 0;
+    int i = 0;
+    int start_location = 1;
+    string abc = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+    cout << "Enter the height: " << endl;
+
+    cin >> height;
+
+    // Input validation
+    if (height <= 0)
+    {
+        cout << "Invalid input." << endl;
+        return 1;
+    }
+
+    // Alogorithm to make the triangle
+    for(int i = 0; i < height; i++)    
+    {
+        if ( i == 0)
+        {
+            cout << 'a' << endl;
+        }
+        else
+        {
+            cout << abc.substr(start_location, start_location + 2) << endl;
+            start_location = (start_location + (3));
+
+            cout << start_location << endl;
+        }
+    }
+}
